a502a9322ce48a2f81ec43f250fafc71a4e8f1ee
Tuomas Laalo <bhm500@myy-haaga-helia.fi>
2025-10-19 20:57:46 +0300

    Added

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const quotes = require('./quotes');
 
 const app = express();
 
-app.get('/quote',(req,res)=>{
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    res.json({quote: quotes[randomIndex]});
+app.get('/quote', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  res.json({ quote: "Day's quote: " + quotes[randomIndex] });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
